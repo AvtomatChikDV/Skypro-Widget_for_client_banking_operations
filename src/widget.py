b86c4166cdefced85497be10fe11164d8e6797d1
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-10-30 16:03:00 +0300

    Add new func get_date in modul widget

--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,10 @@ def mask_account_card(card_or_account_data: str) -> str:
     else:
         new_list[-1] = get_mask_card_number(new_list[-1])
     return " ".join(new_list)
+
+
+def get_date(data_info: str) -> str:
+    """Функция берёт данные даты и времени в формате "2024-03-11T02:26:18.671407
+    и возвращает только дату в формате 'ДД.ММ.ГГГГ' """
+    new_list = data_info[:10]
+    return ".".join(new_list.split("-")[::-1])
