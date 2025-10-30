ae4d2bfacd4db8743dfdec085a35b474ac9c1d5b
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-10-30 16:45:27 +0300

    Update func get_data(use re)

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-
+import re
 
 def mask_account_card(card_or_account_data: str) -> str:
     """Определяет что на входе карта или счёт и выводит соответствующую маску"""
@@ -13,7 +13,9 @@ def mask_account_card(card_or_account_data: str) -> str:
 
 
 def get_date(data_info: str) -> str:
-    """Функция берёт данные даты и времени в формате "2024-03-11T02:26:18.671407
-    и возвращает только дату в формате 'ДД.ММ.ГГГГ' """
-    new_list = data_info[:10]
-    return ".".join(new_list.split("-")[::-1])
+    """Функция берёт данные даты и времени в формате "2024-03-11T02:26:18.671407"
+    и возвращает только дату в формате 'ДД.ММ.ГГГГ'"""
+    # new_list = data_info[:10]
+    pattern = r"(\d{4})-(\d{2})-(\d{2})"
+    match = re.search(pattern, data_info)
+    return ".".join(match.group().split("-")[::-1]) if match else None
