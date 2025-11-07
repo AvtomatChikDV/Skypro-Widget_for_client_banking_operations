f260eaf75f9f5444a31693ad71d2512c81840063
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-11-07 17:52:43 +0300

    add func filter_by_state to homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
-def filter_by_state(processing: list[{}], state: str = 'EXECUTED') -> list[{}]:
-
-    return
-
+def filter_by_state(processing: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция фильтрует список операций по статусу выполнения.
+    Переменная processing - список библиотек формата:
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'} и т.д.]"""
+    return [item for item in processing if item["state"] == state]
