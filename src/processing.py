911d4a45c06d82b5f1a0f5ffafeaa9f3164f1347
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-11-13 18:40:45 +0300

    change_1 widget

--- src/processing.py
+++ src/processing.py
@@ -5,6 +5,8 @@ def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dic
     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'} и т.д.]
     """
+
+
     return [item for item in operations if item["state"] == state]
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 import re
+from datetime import datetime
 
 
 def mask_account_card(card_or_account_data: str) -> str:
     """Определяет что на входе карта или счёт и выводит соответствующую маску"""
     new_list = card_or_account_data.split()
+
+    if new_list == []:
+        raise ValueError("Нет данных!")
 
     if "Счет" in new_list[0]:
         new_list[-1] = get_mask_account(new_list[-1])
@@ -16,7 +20,19 @@ def mask_account_card(card_or_account_data: str) -> str:
 def get_date(data_info: str) -> str:
     """Функция берёт данные даты и времени в формате "2024-03-11T02:26:18.671407"
     и возвращает только дату в формате 'ДД.ММ.ГГГГ'"""
-    # new_list = data_info[:10]
     pattern = r"(\d{4})-(\d{2})-(\d{2})"
     match = re.search(pattern, data_info)
-    return ".".join(match.group().split("-")[::-1]) if match else "Нет даты!!!"
+
+    if not match:
+        return "Неправильная дата!!!"
+
+    # Преобразуем формат из ГГГГ-ММ-ДД в ДД.ММ.ГГГГ
+    date_str = ".".join(match.group().split("-")[::-1])
+
+    try:
+        # Проверяем валидность даты
+        datetime.strptime(date_str, '%d.%m.%Y')
+        return date_str
+    except ValueError:
+        return "Неправильная дата!!!"
+
